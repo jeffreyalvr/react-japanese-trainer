fdeeb1025734dec81ac8890d7a0c183c4f6719a1
Jeffrey Ramo <matheusunitt@gmail.com>
2024-03-10 02:35:34 -0300

    Separa o conteúdo da tab

--- src/components/Alphabets/index.tsx
+++ src/components/Alphabets/index.tsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 
 import Tabs from "./Tabs";
+import TabContent from "./TabContent";
 
 import { TabContext } from "../../Contexts/TabContext";
 import { TranslationsContext } from "../../Contexts/TranslationsContext";
@@ -33,28 +34,10 @@ const Alphabet = () => {
     >
       <div className="rounded-lg flex flex-col w-full border-4 border-[var(--border-box-light)] bg-[var(--bg-box-light)] overflow-hidden dark:border-[var(--border-box-dark)] dark:bg-[var(--bg-box-dark)]">
         <Tabs />
-        <div className="flex flex-row gap-4 p-6 flex-wrap py-6">
-          {alfabeto.hiragana.caracteres.map((item) => (
-            <div className="flex flex-col gap-3">
-              <div
-                key={item.id}
-                className="rounded-lg flex flex-col items-center justify-center p-4 size-20 hover:text-white border-4 border-[var(--border-box-light)] bg-[var(--bg-box-light)] dark:border-[var(--border-box-dark)] dark:bg-[var(--bg-box-dark)] cursor-pointer hover:bg-[var(--primary)]"
-              >
-                <div className="text-xl font-bold text-[var(--fg-light) dark:text-[var(--fg-dark)]">
-                  {item.caractere}
-                </div>
-                {showTranslations ? (
-                  <div className="text-sm text-[var(--primary-opaque)] dark:text-[var(--fg-dark)]">
-                    {item.traducao}
-                  </div>
-                ) : null}
-              </div>
-              <div className="rounded-lg w-full h-1 bg-gray-300 dark:bg-[var(--primary-opaque)]">
-                <div className="rounded-lg w-[30px] h-1 bg-[var(--gold)]"></div>
-              </div>
-            </div>
-          ))}
-        </div>
+        <TabContent
+          alphabet={selectedTab === 0 ? alfabeto.hiragana : alfabeto.katakana}
+          showTranslations={showTranslations}
+        />
       </div>
     </TabContext.Provider>
   );
