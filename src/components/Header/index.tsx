e82c57288a816a617417de697eba3e58fd7c993e
Jeffrey Ramo <matheusunitt@gmail.com>
2024-03-10 23:20:40 -0300

    Altera responsividade do Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,8 +3,8 @@ import logo_img from "../../assets/icons/logo.png";
 const Header = () => {
   return (
     <header className="w-full h-32 flex flex-col p-20 items-center justify-center gap-2 text-center sm:flex-row sm:gap-4">
-      <img src={logo_img} alt="あ" />
-      <h1 className="text-3xl">Japanese Trainer</h1>
+      <img src={logo_img} alt="あ" className="size-12 sm:size-max" />
+      <h1 className="text-2xl sm:text-3xl">Japanese Trainer</h1>
     </header>
   );
 };
