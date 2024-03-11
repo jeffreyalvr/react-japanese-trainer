37f13fd0a8a32f287136a2505ef9ddf8fac74b11
Jeffrey Ramo <matheusunitt@gmail.com>
2024-03-11 00:41:39 -0300

    Adiciona estilização dark no footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="w-full flex flex-col mt-8 gap-2 items-center justify-center py-10 border-t-2 border-dashed border-t-[var(--border-box-light)] dark:border-t-[var(--border-box-dark)]">
-      <span className="flex flex-row gap-2">
+      <span className="flex flex-row gap-2 text-[var(--fg-light)] dark:text-[var(--fg-dark)]">
         Feito por{" "}
         <a
           href="https://jeffreyalvr.dev"
