9a5ba1e6d63a61d1809e3ee528cab70e378441ed
Jeffrey Ramo <matheusunitt@gmail.com>
2024-03-11 00:12:51 -0300

    Realoca os toggles de tema e exibir tradução para o Header; Reorganiza a responsividade do Header

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,15 +1,4 @@
-import { useContext } from "react";
-import { ThemeContext } from "../../Contexts/ThemeContext";
-import { TranslationsContext } from "../../Contexts/TranslationsContext";
-
-import toggle_on_img from "../../assets/icons/toggle_on.png";
-import toggle_off_img from "../../assets/icons/toggle_off.png";
-
 const Footer = () => {
-  const { tema, toggleTema } = useContext(ThemeContext);
-  const { showTranslations, toggleTranslations } =
-    useContext(TranslationsContext);
-
   return (
     <footer className="w-full flex flex-col mt-8 gap-2 items-center justify-center py-10 border-t-2 border-dashed border-t-[var(--border-box-light)] dark:border-t-[var(--border-box-dark)]">
       <span className="flex flex-row gap-2">
@@ -24,29 +13,6 @@ const Footer = () => {
           @jeffreyalvr.
         </a>
       </span>
-      <div className="flex flex-col w-full items-center py-4 md:flex-row md:gap-10 md:justify-center">
-        <button
-          onClick={toggleTema}
-          title="Clique para alterar o tema"
-          className="flex flex-row gap-2 items-center text-[var(--primary-light)] hover:brightness-125"
-        >
-          <img
-            src={tema === "dark" ? toggle_on_img : toggle_off_img}
-            alt="Toggle"
-          />
-          Usar modo escuro
-        </button>
-        <button
-          onClick={toggleTranslations}
-          className="flex flex-row gap-2 items-center text-[var(--primary-light)] hover:brightness-125"
-        >
-          <img
-            src={showTranslations ? toggle_on_img : toggle_off_img}
-            alt="Toggle"
-          />
-          Exibir traduções
-        </button>
-      </div>
     </footer>
   );
 };
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,48 @@
+import { useContext } from "react";
+import { ThemeContext } from "../../Contexts/ThemeContext";
+import { TranslationsContext } from "../../Contexts/TranslationsContext";
+
 import logo_img from "../../assets/icons/logo.png";
+import toggle_on_img from "../../assets/icons/toggle_on.png";
+import toggle_off_img from "../../assets/icons/toggle_off.png";
 
 const Header = () => {
+  const { tema, toggleTema } = useContext(ThemeContext);
+  const { showTranslations, toggleTranslations } =
+    useContext(TranslationsContext);
+
   return (
-    <header className="w-full h-32 flex flex-col p-20 items-center justify-center gap-2 text-center sm:flex-row sm:gap-4">
-      <img src={logo_img} alt="あ" className="size-12 sm:size-max" />
-      <h1 className="text-2xl sm:text-3xl">Japanese Trainer</h1>
+    <header className="flex flex-col gap-10 w-full justify-between py-10 sm:flex-row">
+      <div className="flex flex-col items-center justify-center gap-2 text-center sm:flex-row sm:gap-4">
+        <div className="rounded-md p-2 size-12 sm:size-[64px] bg-[var(--border-box-light)] dark:bg-[var(--border-box-dark)]">
+          <img src={logo_img} alt="あ" className="size-max" />
+        </div>
+        <h1 className="text-2xl sm:text-3xl">Japanese Trainer</h1>
+      </div>
+
+      <div className="flex flex-col justify-center items-center sm:items-end">
+        <button
+          onClick={toggleTema}
+          title="Clique para alterar o tema"
+          className="flex flex-row gap-2 items-center text-[var(--primary-light)] hover:brightness-125"
+        >
+          <img
+            src={tema === "dark" ? toggle_on_img : toggle_off_img}
+            alt="Toggle"
+          />
+          Usar modo escuro
+        </button>
+        <button
+          onClick={toggleTranslations}
+          className="flex flex-row gap-2 items-center text-[var(--primary-light)] hover:brightness-125"
+        >
+          <img
+            src={showTranslations ? toggle_on_img : toggle_off_img}
+            alt="Toggle"
+          />
+          Exibir traduções
+        </button>
+      </div>
     </header>
   );
 };
