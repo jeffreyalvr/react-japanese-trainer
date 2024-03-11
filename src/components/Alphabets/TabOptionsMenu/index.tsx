bc6b5dc26873ca865b3040c97b0e8447ea37e26b
Jeffrey Ramo <matheusunitt@gmail.com>
2024-03-10 23:26:42 -0300

    Altera responsividade do TabOptionsMenu

--- src/components/Alphabets/TabOptionsMenu/index.tsx
+++ src/components/Alphabets/TabOptionsMenu/index.tsx
@@ -2,7 +2,7 @@ import Button from "../../Button";
 
 const TabOptionsMenu = () => {
   return (
-    <div className="flex flex-row justify-center gap-2 w-full p-6 border-b-2 border-[var(--border-box-light)] dark:border-[var(--border-box-dark)]">
+    <div className="flex flex-col justify-center gap-2 w-full p-6 border-b-2 border-[var(--border-box-light)] dark:border-[var(--border-box-dark)] xs:flex-row">
       <Button
         text="Praticar alfabeto"
         variant="primary"
