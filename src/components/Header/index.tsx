86aef66a0994cfa44b7cb60e303b523c60dfc2f1
Jeffrey Ramo <matheusunitt@gmail.com>
2024-03-11 22:41:56 -0300

    Remove imagem de logo e substitui por texto; Reorganiza css global

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,6 @@ import { useContext } from "react";
 import { ThemeContext } from "../../Contexts/ThemeContext";
 import { RomanizationContext } from "../../Contexts/RomanizationContext";
 
-import logo_img from "../../assets/icons/logo.png";
 import toggle_on_img from "../../assets/icons/toggle_on.png";
 import toggle_off_img from "../../assets/icons/toggle_off.png";
 
@@ -14,10 +13,14 @@ const Header = () => {
   return (
     <header className="flex flex-col gap-10 w-full justify-between py-10 sm:flex-row">
       <div className="flex flex-col items-center justify-center gap-2 text-center sm:flex-row sm:gap-4">
-        <div className="rounded-md p-2 size-12 sm:size-[64px] bg-[var(--border-box-light)] dark:bg-[var(--border-box-dark)]">
-          <img src={logo_img} alt="あ" className="size-full" />
+        <div className="flex flex-col items-center justify-center rounded-md size-12 sm:size-[64px] bg-[var(--border-box-light)] dark:bg-[var(--border-box-dark)]">
+          <span className="font-bold text-3xl text-transparent bg-gradient-to-br from-[var(--primary)] to-[var(--gold)] bg-clip-text sm:text-[2.5rem]">
+            あ
+          </span>
         </div>
-        <h1 className="text-2xl sm:text-3xl">Japanese Trainer</h1>
+        <h1 className="font-bold text-2xl text-transparent bg-gradient-to-br from-[var(--primary)] to-[var(--gold)] bg-clip-text sm:text-3xl">
+          Japanese Trainer
+        </h1>
       </div>
 
       <div className="flex flex-col justify-center items-center sm:items-end">
