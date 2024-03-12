30f3e82347c91b8160ee17265b304c38de780f8c
Jeffrey Ramo <matheusunitt@gmail.com>
2024-03-11 22:54:44 -0300

    Cria página de not found

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,5 +1,19 @@
+import Header from "../../components/Header";
+import Box from "../../components/Box";
+import { LinkButton } from "../../components/Button";
+import Footer from "../../components/Footer";
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <>
+      <Header />
+      <Box title="Página não encontrada">
+        <span>A página procurada não está em nosso sitema.</span>
+        <LinkButton text="Voltar ao início" route="/" />
+      </Box>
+      <Footer />
+    </>
+  );
 };
 
 export default NotFound;
