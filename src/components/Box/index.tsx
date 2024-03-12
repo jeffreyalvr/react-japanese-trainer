85287894b5bfc389f538b0810ded33aa50d6c9c5
Jeffrey Ramo <matheusunitt@gmail.com>
2024-03-11 22:42:19 -0300

    Adiciona conteúdo reutilizavel no componente Box

--- src/components/Box/index.tsx
+++ src/components/Box/index.tsx
@@ -1,13 +1,16 @@
-import Button from "../Button";
-
-const Box = () => {
+const Box = ({
+  title,
+  children,
+}: {
+  title: string;
+  children: React.ReactNode;
+}) => {
   return (
-    <div className="rounded-lg w-full p-10 border-4 border-[var(--border-box-light)] bg-[var(--bg-box-light)] dark:border-[var(--border-box-dark)] dark:bg-[var(--bg-box-dark)]">
-      <span>Teste de BOX</span>
-      <div className="flex flex-row gap-2 py-4">
-        <Button text="teste" title="hover" variant="primary" />
-        <Button text="teste" title="hover" variant="secondary" />
+    <div className="rounded-lg w-full flex flex-col gap-6 p-10 border-4 border-[var(--border-box-light)] bg-[var(--bg-box-light)] dark:border-[var(--border-box-dark)] dark:bg-[var(--bg-box-dark)]">
+      <div className="pb-4 border-b-2 border-dashed border-[var(--border-box-light)] dark:border-[var(--border-box-dark)]">
+        <h1 className="text-3xl">{title}</h1>
       </div>
+      <div className="">{children}</div>
     </div>
   );
 };
