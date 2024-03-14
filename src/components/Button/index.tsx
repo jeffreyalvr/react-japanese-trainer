850cd8b43f5609e5447acaf2f959c5e333f933f0
Jeffrey Ramo <matheusunitt@gmail.com>
2024-03-13 23:57:06 -0300

    Reorganiza Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
 type DefaultProps = {
-  text: string;
+  text?: string;
   title?: string;
 };
 
@@ -9,24 +9,44 @@ type ButtonProps = DefaultProps & {
   variant: "primary" | "secondary";
 };
 
+type IconButtonProps = DefaultProps &
+  ButtonProps & {
+    icon: string;
+    fillColor: string;
+  };
+
 type LinkButtonProps = DefaultProps & {
   route: string;
 };
+
+const defaultCss = "border-2 rounded-md px-4 py-1 h-[36px] cursor-pointer";
+const primaryCss =
+  "bg-[#6562fc] border-transparent hover:bg-[#7472ff] text-white";
+const secondaryCss =
+  "bg-[#e2e2e2] border-gray-300 hover:bg-[#f4f4f4] text-black";
 
 const Button = ({ text, title, variant }: ButtonProps) => {
   return (
     <button
       title={title || ""}
-      className={`
-        border-2 rounded-md px-4 py-1 cursor-pointer
-        ${
-          variant == "primary"
-            ? "bg-[#6562fc] border-transparent hover:bg-[#7472ff] text-white"
-            : "bg-[#e2e2e2] border-gray-300 hover:bg-[#f4f4f4] text-black"
-        }
-          `}
+      className={`${defaultCss} ${
+        variant == "primary" ? primaryCss : secondaryCss
+      }`}
     >
       {text || "botão"}
+    </button>
+  );
+};
+
+const IconButton = ({ title, variant, icon, fillColor }: IconButtonProps) => {
+  return (
+    <button
+      title={title || ""}
+      className={`${defaultCss} ${
+        variant == "primary" ? primaryCss : secondaryCss
+      }`}
+    >
+      <svg src={icon} alt="Ícone" className={`size-full fill-[${fillColor}]`} />
     </button>
   );
 };
@@ -36,11 +56,11 @@ const LinkButton = ({ text, title, route }: LinkButtonProps) => {
     <Link
       to={route}
       title={title || ""}
-      className="border-2 rounded-md px-4 py-1 cursor-pointer bg-[#6562fc] border-transparent hover:bg-[#7472ff] text-white"
+      className={`${defaultCss + primaryCss}`}
     >
       {text || "botão"}
     </Link>
   );
 };
 
-export { Button, LinkButton };
+export { Button, IconButton, LinkButton };
