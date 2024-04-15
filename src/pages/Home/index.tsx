dff273edf7ab1edb66dc5c4a1c12514ef7dc67f5
Jeffrey Ramo <matheusunitt@gmail.com>
2024-04-15 09:12:37 -0300

    Remove context da página Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,14 +3,10 @@ import Footer from "../../components/Footer";
 import Alphabet from "../../components/Alphabets";
 import Modal from "../../components/Modal";
 
-import { useModalContext } from "../../Contexts/ModalContext";
-
 const Home = () => {
-  const { modalActive } = useModalContext();
-
   return (
     <>
-      <Modal isOpen={modalActive} />
+      <Modal />
       <div className="container flex flex-col gap-2 mx-auto md:w-[40rem]">
         <Header />
         <Alphabet />
