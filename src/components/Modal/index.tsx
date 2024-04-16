958ffa26970bc030f99351da4e04c483972ef434
Jeffrey Ramo <matheusunitt@gmail.com>
2024-04-16 15:57:13 -0300

    Reorganiza modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -5,9 +5,20 @@ import { useModalContext } from "../../Contexts/ModalContext";
 
 const Modal = () => {
   const { modalActive, type } = useModalContext();
+  const renderModalComponent = () => {
+    switch (type) {
+      case "LEARNING_MODAL":
+        return <LearningModal />;
+      case "DETAILS_MODAL":
+        return <DetailsModal />;
+      default:
+        return "Nenhum componente definido.";
+    }
+  };
+
   return modalActive ? (
     <div className="absolute flex flex-col gap-2 justify-center items-center size-full bg-[var(--modal-light-bg)] dark:bg-[var(--modal-dark-bg)] backdrop-blur-sm">
-      {type === "LEARNING_MODAL" ? <LearningModal /> : <DetailsModal />}
+      {renderModalComponent()}
     </div>
   ) : null;
 };
